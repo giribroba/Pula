27d974836432cee0cb0a39f64a6e3571a7d24fea
giribroba <giribroba@gmail.com>
2019-10-29 04:15:57 -0300

    Fazendo tabela

---
+++ Assets/Scripts/Liga/Teste.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teste : MonoBehaviour
+{
+    [SerializeField] private LayerMask tabela;
+    private Vector2 posição;
+
+    void Update()
+    {
+        posição = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+}
