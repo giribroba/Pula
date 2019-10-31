60ba8acb12fb3d49376d3b7b0e1581cf23a9f9a9
Vitor <gabrielaragaofds@gmail.com>
2019-10-31 11:57:25 -0300

    Mover camera

---
+++ Assets/Scripts/Liga/Controlaveis/Cenário.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cenário : MonoBehaviour
+{
+    [SerializeField] float velocidade;
+    [SerializeField] float camMaxX;
+    void Update()
+    {
+        if (Input.touchCount == 2 && Mathf.Abs(Camera.main.transform.position.x) <= camMaxX)
+        {
+            Camera.main.transform.Translate(Vector2.left * Input.touches[0].deltaPosition.x * velocidade * Time.deltaTime);
+        }
+        if (Mathf.Abs(Camera.main.transform.position.x) > camMaxX)
+        {
+            Camera.main.transform.position = new Vector3((Camera.main.transform.position.x < 0? -camMaxX : camMaxX), Camera.main.transform.position.y, Camera.main.transform.position.z);
+        }
+    }
+}
