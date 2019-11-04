9565cb45ca78a1748a9a25861d469c9297ed5fb4
giribroba <giribroba@gmail.com>
2019-11-04 00:54:51 -0300

    canos posicionaveis

--- Assets/Scripts/Liga/Controlaveis/Cenário.cs
+++ Assets/Scripts/Liga/Controlaveis/Cenário.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Cenário : MonoBehaviour
 {
+    public static RaycastHit2D[] hit;
+    public static Canobotao selecionado;
+
+    [SerializeField] GameObject selecionadoBorda, canoReto, canoL;
     [SerializeField] float velocidade;
     [SerializeField] float camMaxX;
     void Update()
@@ -16,5 +20,31 @@ public class Cenário : MonoBehaviour
         {
             Camera.main.transform.position = new Vector3((Camera.main.transform.position.x < 0? -camMaxX : camMaxX), Camera.main.transform.position.y, Camera.main.transform.position.z);
         }
+
+        if (selecionado != null)
+        {
+            selecionadoBorda.transform.position = selecionado.gameObject.transform.position;
+        }
+
+        if (Input.touchCount == 1)
+        {
+            hit = Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.forward);
+            foreach (var item in hit)
+            {
+                if (selecionado != null && selecionado.quantidade > 0 && item.collider.gameObject.tag ==  "Tabela")
+                {
+                    switch (selecionado.tipoCano)
+                    {
+                        case Canobotao.Tipo.L:
+                            Instantiate(canoL, item.transform.position, selecionado.gameObject.transform.rotation);
+                            break;
+                        case Canobotao.Tipo.Reto:
+                            Instantiate(canoReto, item.transform.position, selecionado.gameObject.transform.rotation);
+                            break;
+                    }
+                }
+            }
+        }
     }
+
 }
