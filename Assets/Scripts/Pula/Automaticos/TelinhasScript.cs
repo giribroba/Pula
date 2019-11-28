868870c4ca6c6fa98220477ab599c91f3da338f9
giribroba <giribroba@gmail.com>
2019-11-28 10:50:35 -0300

    musica over

--- Assets/Scripts/Pula/Automaticos/TelinhasScript.cs
+++ Assets/Scripts/Pula/Automaticos/TelinhasScript.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 public class TelinhasScript : MonoBehaviour
 {
     [SerializeField] private GameObject botaoPause, pauseMenu;
+    [SerializeField] private AudioSource gameplay, gameOver;
     public IEnumerator Desce(string tela)
     {
+        gameplay.enabled = false;
+        gameOver.enabled = true;
         botaoPause.SetActive(false);
         pauseMenu.SetActive(false);
         this.transform.GetChild(0).gameObject.SetActive(tela == "over");
